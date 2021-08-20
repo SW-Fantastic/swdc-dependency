fd69c7660069abe3f00dbd8c224b846e02884544
SW-Fantastic <396146982@qq.com>
2021-08-20 18:12:19 +0800

    update

--- src/main/java/org/swdc/dependency/annotations/MultipleImplement.java
+++ src/main/java/org/swdc/dependency/annotations/MultipleImplement.java
@@ -1,12 +1,15 @@
 package org.swdc.dependency.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 多实现注解，标记在某一个
  * 抽象类或者接口的具体实现类上面
  */
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MultipleImplement {
 
